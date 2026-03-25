017f1a27e3220fadada5195f02caec675473795e
HannaGreentree <s2327926@students.ncl-coll.ac.uk>
2026-03-25 22:31:58 +0000

    Fix allowed hosts

--- openpaws_project/settings.py
+++ openpaws_project/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]  # for now (we can tighten later)
+ALLOWED_HOSTS = ["*"]
 
 # APPLICATIONS
 INSTALLED_APPS = [
