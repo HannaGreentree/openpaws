da30e5675f84887b931d97acb5fd1df5b2f45a49
HannaGreentree <s2327926@students.ncl-coll.ac.uk>
2026-03-26 13:54:04 +0000

    Use DATABASE_URL on Render

--- openpaws_project/settings.py
+++ openpaws_project/settings.py
@@ -1,10 +1,11 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
@@ -57,10 +58,10 @@ TEMPLATES = [
 WSGI_APPLICATION = "openpaws_project.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
