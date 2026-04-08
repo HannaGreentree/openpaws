9082d4c38fd415a71ac476c31e491957ed2a0ee4
HannaGreentree <s2327926@students.ncl-coll.ac.uk>
2026-04-08 21:35:09 +0100

    Fix shelter_list missing and thank you balance calculation

--- shelters/views.py
+++ shelters/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Sum
 
 from .models import Shelter
+from payments.models import Donation
+from cases.models import Case
 
 
 def shelter_list(request):
@@ -10,13 +13,22 @@ def shelter_list(request):
 
 def shelter_detail(request, pk):
     shelter = get_object_or_404(Shelter, pk=pk)
-    cases = shelter.cases.all()
 
-    return render(
-        request,
-        "shelters/shelter_detail.html",
-        {
-            "shelter": shelter,
-            "cases": cases,
-        },
+    thank_you_balance = (
+        Donation.objects.filter(
+            shelter=shelter,
+            paid=True,
+            donation_type="THANK_YOU",
+        ).aggregate(total=Sum("amount"))["total"]
+        or 0
     )
+
+    cases = Case.objects.filter(shelter=shelter)
+
+    context = {
+        "shelter": shelter,
+        "cases": cases,
+        "thank_you_balance": thank_you_balance,
+    }
+
+    return render(request, "shelters/shelter_detail.html", context)
