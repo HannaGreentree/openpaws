bcc67ca2dd030eb8a39bb3d6bfb9ca037d365891
HannaGreentree <s2327926@students.ncl-coll.ac.uk>
2026-04-09 01:07:07 +0100

    Update shelter model, image, social links, privacy changes

--- shelters/admin.py
+++ shelters/admin.py
@@ -7,4 +7,20 @@ class ShelterAdmin(admin.ModelAdmin):
     list_display = ("name", "country", "city", "trust_points", "is_verified")
     list_filter = ("country", "is_verified")
     search_fields = ("name", "city", "country", "email")
-    readonly_fields = ("trust_points",)  # IMPORTANT: stops manual editing
+    readonly_fields = ("trust_points",)
+
+    fields = (
+        "owner",
+        "name",
+        "country",
+        "city",
+        "address",
+        "email",
+        "phone",
+        "animals_count",
+        "social_link_1",
+        "social_link_2",
+        "image",
+        "trust_points",
+        "is_verified",
+    )
--- shelters/models.py
+++ shelters/models.py
@@ -4,14 +4,21 @@ from django.contrib.auth.models import User
 
 class Shelter(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
     name = models.CharField(max_length=200)
     country = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     address = models.CharField(max_length=255)
     email = models.EmailField()
     phone = models.CharField(max_length=50)
+
     animals_count = models.IntegerField(default=0)
-    social_link = models.URLField(blank=True)
+
+    social_link_1 = models.URLField(default="https://example.com")
+    social_link_2 = models.URLField(default="https://example.com")
+
+    image = models.ImageField(upload_to="shelters/", blank=True, null=True)
+
     trust_points = models.IntegerField(default=0)
     is_verified = models.BooleanField(default=False)
 
